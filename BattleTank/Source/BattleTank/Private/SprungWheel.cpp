1968541bdf70c0e49a52eebd7fd59392040f84f6
chris <christopherparello@gmail.com>
2019-03-17 02:15:29 -0700

    NOW BUG ON LAUNCH TARRAY   THEN BUILD FAILED ALL TOGETHER

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,45 @@
+// Copyright Chris Parello
+
+#include "SprungWheel.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheelConstraint"));
+	SetRootComponent(MassWheelConstraint);
+
+	
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	Wheel->SetupAttachment(MassWheelConstraint);
+
+	
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("not null"))
+	}else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("null"))
+	}
+	
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
